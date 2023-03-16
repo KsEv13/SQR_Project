0b8ceefbe55906a313f7f9603a8f7285569f6cb3
KsEv13 <k.evdokimova@innopolis.university>
2023-03-16 05:36:23 +0300

    Linting test

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -8,10 +8,10 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/login">
+        <Route exact path="/login">
           <Login />
         </Route>
-        <Route exact path="/register">
+        <Route exact path='/register">
           <Register />
         </Route>
         <Route exact path="/home">
